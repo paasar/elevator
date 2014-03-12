ebadd08254e301d8c51288f78ff16b1af5cd33ae
paasar <paasar@gmail.com>
2014-03-12 14:42:30 +0200

    State and floor instantiation.

--- elevator-server/src/elevator_server/core.clj
+++ elevator-server/src/elevator_server/core.clj
@@ -4,8 +4,21 @@
             [compojure.route :as route]
             [cheshire.core :as json]))
 
+(def template-content
+  (json/parse-string (slurp "resources/state-template.json") true))
+
+(defn create-floor [floor-number]
+  (let [floor-template (first (template-content :floors))]
+    (assoc-in floor-template [:number] floor-number)))
+
+(defn create-floors [number-of-floors]
+  (map #(create-floor %) (range 1 (inc number-of-floors))))
+
+(defn create-new-state-data []
+    (assoc-in template-content [:floors] (create-floors 9)))
+
 (defroutes app-routes
-  (GET "/" [] (str (json/parse-string (slurp "resources/state.json") true)))
+  (GET "/" [] (str (create-new-state-data)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
--- elevator-server/test/elevator_server/core_test.clj
+++ elevator-server/test/elevator_server/core_test.clj
@@ -5,11 +5,28 @@
         elevator-server.core))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+;  (testing "main route"
+;    (let [response (app (request :get "/"))]
+;      (is (= (:status response) 200))
+;      (is (= (:body response) "Hello World"))))
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+(deftest data-handling
+  (testing "floor creation"
+    (let [fifth-floor (create-floor 5)
+          sixth-floor (create-floor 6)]
+      (is (= (:number fifth-floor) 5))
+      (is (= (:waiting fifth-floor) 0))
+      (is (= (:impatient fifth-floor) 0))
+      (is (= (:number sixth-floor) 6))
+      (is (= (:waiting sixth-floor) 0))
+      (is (= (:impatient sixth-floor) 0))))
+
+  (testing "floors creation"
+    (let [nine-floors (create-floors 9)
+          seven-floors (create-floors 7)]
+      (is (= (count nine-floors) 9))
+      (is (= (count seven-floors) 7)))))
