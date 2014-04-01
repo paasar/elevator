f01d04fb4bda284c7bbf7c28b223b0bc2f668bac
paasar <paasar@gmail.com>
2014-04-01 20:59:05 +0300

    Moving elevator around and through different states.

--- elevator-server/test/elevator_server/data_test.clj
+++ elevator-server/test/elevator_server/data_test.clj
@@ -81,6 +81,12 @@
           state-after-step (advance-player-state start-player-state)]
       (is (= 1 (get-in state-after-step [:tally :unhappy]))))))
 
+(defn create-state-with-defined-elevator [state current-floor target-floor]
+  (-> (create-new-player-state)
+    (assoc-in [:elevator :state] state)
+    (assoc-in [:elevator :current-floor] current-floor)
+    (assoc-in [:elevator :going-to] target-floor)))
+
 (deftest elevator-states
   (testing "same floor gives waiting"
     (is (= :waiting (get-ascending-or-descending-or-waiting 1 1))))
@@ -95,4 +101,49 @@
           elevator (get after-state :elevator)]
       (is (= 1 (:current-floor elevator)))
       (is (= 2 (:going-to elevator)))
-      (is (= :ascending (:state elevator))))))
+      (is (= :ascending (:state elevator)))))
+
+  (testing "move elevator up, not reaching target"
+    (let [before-state (create-state-with-defined-elevator :ascending 1 3)
+          after-state (update-elevator-state before-state)
+          elevator (get after-state :elevator)]
+      (is (= 2 (:current-floor elevator)))
+      (is (= :ascending (:state elevator)))))
+
+  (testing "move elevator up, reaching target, no riders, no newcomers"
+    (let [before-state (create-state-with-defined-elevator :ascending 2 3)
+          after-state (update-elevator-state before-state)
+          elevator (get after-state :elevator)]
+      (is (= 3 (:current-floor elevator)))
+      (is (= :waiting (:state elevator)))))
+
+  ;(testing "move elevator up, reaching target, riders")
+  ;(testing "move elevator up, reaching target, no riders, newcomers")
+
+  (testing "move elevator down, not reaching target"
+    (let [before-state (create-state-with-defined-elevator :descending 3 1)
+          after-state (update-elevator-state before-state)
+          elevator (get after-state :elevator)]
+      (is (= 2 (:current-floor elevator)))
+      (is (= :descending (:state elevator)))))
+
+  (testing "move elevator down, reaching target, no riders, no newcomers"
+    (let [before-state (create-state-with-defined-elevator :descending 2 1)
+          after-state (update-elevator-state before-state)
+          elevator (get after-state :elevator)]
+      (is (= 1 (:current-floor elevator)))
+      (is (= :waiting (:state elevator)))))
+
+  ;(testing "move elevator down, reaching target, riders")
+  ;(testing "move elevator down, reaching target, no riders, newcomers")
+
+  (testing "move waiting elevator does nothing"
+    (let [before-state (create-state-with-defined-elevator :waiting 1 1)
+          after-state (update-elevator-state before-state)
+          elevator (get after-state :elevator)]
+      (is (= 1 (:current-floor elevator)))
+      (is (= :waiting (:state elevator))))))
+
+  ;(testing "elevator disembarking to embarking")
+  ;(testing "elevator disembarking to waiting (no newcomers)")
+  ;(testing "elevator embarking to waiting"))
