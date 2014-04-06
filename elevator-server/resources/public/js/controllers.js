01d2a24aa783462c28cb17a877b82d4ac2798b29
paasar <paasar@gmail.com>
2014-04-06 16:50:43 +0300

    Beginnings of view structure. State updating added.

--- elevator-server/resources/public/js/controllers.js
+++ elevator-server/resources/public/js/controllers.js
@@ -1,6 +1,10 @@
 var renderApp = angular.module('renderApp', ['stateService']);
 
-renderApp.controller('RenderController', ['$scope', 'GameState', function ($scope, GameState) {
-    //$scope.states = [{"client": {"name": "tiimi 1"}}];
-    $scope.states = GameState.query();
+renderApp.controller('RenderController', ['$scope', '$timeout', 'GameState', function ($scope, $timeout, GameState) {
+    var updateState = function () {
+        $scope.states = GameState.query();
+        $timeout(function() {updateState();}, 5000);
+
+    };
+    $timeout(function() {updateState();}, 5000);
 }]);
