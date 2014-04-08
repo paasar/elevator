b748fc3405fd80428720fa7e8864580ad26b8623
paasar <paasar@gmail.com>
2014-04-08 20:38:28 +0300

    Control panel added to view.

--- elevator-server/src/elevator_server/view.clj
+++ elevator-server/src/elevator_server/view.clj
@@ -14,37 +14,47 @@
   {:state (:state elevator)
    :riders (into to-requests (repeat (- capacity (count to-requests)) "free"))}))
 
-(defn add-elevator-if-same-floor [floor elevator]
+(defn floor-numbers [max-floor]
+  (range max-floor 0 -1))
+
+(defn create-button [number riders]
+  (let [button-on (some #(= number %) riders)]
+    {(keyword (str number)) (if button-on "on" "off")}))
+
+(defn create-control-panel-for-view [riders max-floor]
+  (vec (map #(create-button % riders) (floor-numbers max-floor))))
+
+(defn add-control-panel-and-elevator-if-same-floor [floor elevator max-floor]
   (if (= (:number floor) (:current-floor elevator))
-    (assoc floor :elevator (create-elevator-for-view elevator))
+    (-> floor
+        (assoc :elevator (create-elevator-for-view elevator))
+        (assoc :control-panel (create-control-panel-for-view (:to-requests elevator) max-floor)))
     floor))
 
-(defn add-counter-weight-if-correct-floor [floor floors elevator-floor]
-  (if (= (:number floor) (inc (- floors elevator-floor)))
+(defn add-counter-weight-if-correct-floor [floor max-floor elevator-floor]
+  (if (= (:number floor) (inc (- max-floor elevator-floor)))
     (assoc floor :counter-weight true)
     floor))
 
-(defn create-floor-for-view [number elevator requests floors]
+(defn create-floor-for-view [number elevator requests max-floor]
   (-> {:number number}
-      (add-elevator-if-same-floor elevator)
-      (add-counter-weight-if-correct-floor floors (:current-floor elevator))))
+      (add-control-panel-and-elevator-if-same-floor elevator max-floor)
+      (add-counter-weight-if-correct-floor max-floor (:current-floor elevator))))
 
-(defn create-view-floors [elevator requests floors]
-  (let [floor-numbers (range floors 0 -1)]
-    (vec (map #(create-floor-for-view % elevator requests floors) floor-numbers))
-  ))
+(defn create-view-floors [elevator requests max-floor]
+    (vec (map #(create-floor-for-view % elevator requests max-floor) (floor-numbers max-floor))))
 
 (defn transform-player-state-to-view-data [player-state]
   (let [client (:client player-state)
         elevator (:elevator player-state)
-        floors (:floors player-state)
+        max-floor (:floors player-state)
         requests (:from-requests player-state)
         tally (:tally player-state)]
   (do
     (println "tpstvd" client elevator requests tally)
     (-> {}
       (assoc :tally (add-overall-score tally))
-      (assoc :floors (create-view-floors elevator requests floors))
+      (assoc :floors (create-view-floors elevator requests max-floor))
       (assoc :client {:name (:name client)})
       ))
   )
