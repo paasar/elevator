83d43f5fb7846a06761a0968d621241d2d248404
paasar <paasar@gmail.com>
2014-04-12 17:00:21 +0300

    Improved handler routes so that .html extensions are not needed. Renamed and added submit button to player creation form.

--- elevator-server/src/elevator_server/handler.clj
+++ elevator-server/src/elevator_server/handler.clj
@@ -1,18 +1,29 @@
 (ns elevator-server.handler
   (:use compojure.core
-        elevator-server.core)
+        elevator-server.core
+        ring.util.response)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [elevator-server.view :refer [transform-game-state-to-view-data]]
             [elevator-server.scheduler :as scheduler]
             [cheshire.core :refer [generate-string]]))
 
+(defn file [path]
+  (resource-response path {:root "public"}))
+
 (defroutes app-routes
-  (GET "/state/internal" [] (generate-string (get-game-state)))
+  (GET "/" [] (file "player-creation.html"))
+  (POST "/player" [post-data] "TODO create new player")
+
+  (GET "/game" [] (file "game.html"))
+
   (GET "/state" [] (generate-string (transform-game-state-to-view-data (get-game-state))))
+  (GET "/state/internal" [] (generate-string (get-game-state)))
+
+  ;(GET "/admin" [] (file "admin.html")
   (GET "/start" [] "TODO start command")
   (GET "/stop" [] "TODO stop command")
-  (POST "/player" [post-data] "TODO create new player")
+
   (route/resources "/")
   (route/not-found "Not Found"))
 
