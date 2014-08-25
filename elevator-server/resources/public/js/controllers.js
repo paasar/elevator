4c14e6ad90c7c422af982a8e067f53fa30397a77
Ari Paasonen <paasar@gmail.com>
2014-08-25 10:15:34 +0300

    Prevent blinking in game view.

--- elevator-server/resources/public/js/controllers.js
+++ elevator-server/resources/public/js/controllers.js
@@ -1,10 +1,16 @@
 var renderApp = angular.module('renderApp', ['stateService']);
 
-renderApp.controller('RenderController', ['$scope', '$timeout', 'GameState', function ($scope, $timeout, GameState) {
+renderApp.controller('RenderController', ['$scope', '$timeout', 'GameState', '$q', function ($scope, $timeout, GameState, $q) {
     $scope.states = GameState.query();
 
     var updateState = function () {
-        $scope.states = GameState.query();
+         var newStates = GameState.query();
+        // Wait for the response to prevent blinking.
+        $q.all([
+            newStates.$promise
+        ]).then(function() {
+            $scope.states = newStates;
+        });
         $timeout(function() {updateState();}, 1000);
 
     };
