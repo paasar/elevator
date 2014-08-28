58d63cffb8a0369a8d279b44e02ad7cf37cd0f54
Ari Paasonen <paasar@gmail.com>
2014-08-28 08:08:37 +0300

    Finishing touches to look of the elevator. People inside elevator are now smilies. Elevator's digital display shows different states.

--- elevator-server/src/elevator_server/constants.clj
+++ elevator-server/src/elevator_server/constants.clj
@@ -3,7 +3,7 @@
 ;TODO utilize dynamic vars in tests
 (def ^:dynamic *number-of-floors* 5)
 
-(def ^:dynamic *capacity* *number-of-floors*)
+(def ^:dynamic *capacity* (inc *number-of-floors*))
 
 (def ^:dynamic *impatience-start* 5)
 
