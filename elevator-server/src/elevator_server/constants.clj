d47108337ddd0c4d8ebf732a5104adf6108b2a27
Ari Paasonen <paasar@gmail.com>
2014-09-07 14:08:18 +0300

    Small game view tweak and reminder.

--- elevator-server/src/elevator_server/constants.clj
+++ elevator-server/src/elevator_server/constants.clj
@@ -1,5 +1,9 @@
 (ns elevator-server.constants)
 
+; Keep in mind that changing these values will most likely
+; break the game view layout since it's filled with
+; absolute pixel values.
+
 ;TODO utilize dynamic vars in tests
 (def ^:dynamic *number-of-floors* 5)
 
