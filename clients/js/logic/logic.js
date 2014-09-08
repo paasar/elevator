15a77b6611a60b79966ebdc2ca515d5abbfa74bc
Ari Paasonen <paasar@gmail.com>
2014-09-08 08:30:48 +0300

    Separated logic from handler in JS client.

---
+++ clients/js/logic/logic.js
@@ -0,0 +1,47 @@
+/**
+ * The actual logic that decides where to go next.
+ *
+ * Stateless approach is recommended.
+ *
+ * PlayerState example:
+   {"elevator":
+        {"toRequests": [1, 3, 5, 5],
+         "currentFloor": 1,
+         "goingTo": 1,
+         "state": "WAITING",
+         "capacity": 6},
+   "floors": 5,
+   "fromRequests":
+     [  {"floor": 1, "impatient": true, "direction": "UP"}
+     ,  {"floor": 2, "impatient": false, "direction": "DOWN"}
+     ],
+   "tally":
+        {"happy": 0,
+         "unhappy": 0},
+   "tick": 3}
+ */
+var logic = {
+  decideWhichFloorToGo : function(state) {
+    console.log("PlayerState:");
+    console.log(state);
+
+    var currentFloor = state.elevator.currentFloor;
+    var currentTarget = state.elevator.goingTo;
+    var topFloor = state.floors;
+
+    if (currentFloor !== currentTarget) {
+      console.log("I want to go to " + currentTarget + "!");
+      return currentTarget;
+    } else {
+      if (currentFloor === 1) {
+        return topFloor;
+      } else {
+        var oneDown = currentFloor - 1;
+        console.log("Going down (" + oneDown + ")!");
+        return oneDown;
+      }
+    }
+  }
+}
+
+module.exports = logic;
--- clients/js/routes/handler.js
+++ clients/js/routes/handler.js
@@ -1,50 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-//TODO move into own file
-/**
- * The actual logic that decides where to go next.
- *
- * Stateless approach is recommended.
- *
- * PlayerState example:
-   {"elevator":
-        {"toRequests": [1, 3, 5, 5],
-         "currentFloor": 1,
-         "goingTo": 1,
-         "state": "WAITING",
-         "capacity": 6},
-   "floors": 5,
-   "fromRequests":
-     [  {"floor": 1, "impatient": true, "direction": "UP"}
-     ,  {"floor": 2, "impatient": false, "direction": "DOWN"}
-     ],
-   "tally":
-        {"happy": 0,
-         "unhappy": 0},
-   "tick": 3}
- */
-var decideWhichFloorToGo = function(state) {
-  console.log("PlayerState:");
-  console.log(state);
-
-  var currentFloor = state.elevator.currentFloor;
-  var currentTarget = state.elevator.goingTo;
-  var topFloor = state.floors;
-
-  if (currentFloor !== currentTarget) {
-    console.log("I want to go to " + currentTarget + "!");
-    return currentTarget;
-  } else {
-    if (currentFloor === 1) {
-      return topFloor;
-    } else {
-      var oneDown = currentFloor - 1;
-      console.log("Going down (" + oneDown + ")!");
-      return oneDown;
-    }
-  }
-}
+var logic = require('../logic/logic');
 
 /* GET */
 router.get('/', function(req, res) {
@@ -54,7 +11,7 @@ router.get('/', function(req, res) {
 /* POST */
 router.post('/', function(req, res) {
   var state = req.body;
-  res.send({"go-to": decideWhichFloorToGo(state)});
+  res.send({"go-to": logic.decideWhichFloorToGo(state)});
 });
 
 
