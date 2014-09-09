df5e1b609f8f0599f2d3a6e6d0e3c19af04edddc
Ari Paasonen <paasar@gmail.com>
2014-09-09 18:04:36 +0300

    Fixed clj client tests to match new logic.

--- clients/clj/test/elevator/handler_test.clj
+++ clients/clj/test/elevator/handler_test.clj
@@ -14,24 +14,25 @@
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
 
-(defn create-test-status [current-floor]
-  {:elevator {:current-floor current-floor}
+(defn create-test-status [current-floor going-to]
+  {:elevator {:currentFloor current-floor
+              :goingTo going-to}
    :floors 3})
 
-(defn create-post-request [current-floor]
+(defn create-post-request [current-floor going-to]
   (-> (request :post "/")
-      (body (json/generate-string (create-test-status current-floor)))))
+      (body (json/generate-string (create-test-status current-floor going-to)))))
 
 (defn go-to-response [floor]
   (json/generate-string {:go-to floor}))
 
 (deftest test-app-post
   (testing "from one to two"
-    (let [response (app (create-post-request 1))]
-      (is (= (:body response) (go-to-response 2)))))
-  (testing "from two to top"
-    (let [response (app (create-post-request 2))]
+    (let [response (app (create-post-request 1 1))]
       (is (= (:body response) (go-to-response 3)))))
+  (testing "from two to top"
+    (let [response (app (create-post-request 2 1))]
+      (is (= (:body response) (go-to-response 1)))))
   (testing "from top to bottom"
-    (let [response (app (create-post-request 3))]
-      (is (= (:body response) (go-to-response 1))))))
+    (let [response (app (create-post-request 3 2))]
+      (is (= (:body response) (go-to-response 2))))))
