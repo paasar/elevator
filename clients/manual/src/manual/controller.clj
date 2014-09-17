08b60ae3cab6af15cc1784a3f70a5e69a7e4ccda
Ari Paasonen <paasar@gmail.com>
2014-09-17 07:59:55 +0300

    Manual control panel now responds to floor amount given in player state.

--- clients/manual/src/manual/controller.clj
+++ clients/manual/src/manual/controller.clj
@@ -1,9 +1,9 @@
 (ns manual.controller
   (require [cheshire.core :as json]))
 
-(import '(javax.swing JFrame JPanel JButton JOptionPane BoxLayout)
-        'java.awt.event.ActionListener
-        'java.awt.Dimension)
+(import '(javax.swing JFrame JPanel JButton)
+        '(java.awt Dimension GridLayout)
+        'java.awt.event.ActionListener)
 
 (def target (atom 1))
 
@@ -12,36 +12,42 @@
 
 (defn set-target [new-target]
   (do
-    (println (str "Setting " new-target))
+    (println (str "Setting target to " new-target))
     (reset! target new-target)))
 
-(defn say-num [num]
-  (JOptionPane/showMessageDialog
-    nil (str num) "Greeting"
-    JOptionPane/INFORMATION_MESSAGE))
+(def number-of-floors (atom 5))
 
-(defn create-action [num]
+(defn get-number-of-floors []
+  @number-of-floors)
+
+(defn set-number-of-floors [new-number-of-floors]
+  (do
+    (println (str "Setting number of floors to " new-number-of-floors))
+    (reset! number-of-floors new-number-of-floors)))
+
+(defn create-change-target-action [num]
   (proxy [ActionListener] []
     (actionPerformed [event] (set-target num))))
 
 (defn create-button [num]
   (doto (JButton. (str num))
         (.setPreferredSize (Dimension. 100 50))
-        (.addActionListener (create-action num))))
+        (.addActionListener (create-change-target-action num))))
 
 (defn add-buttons [panel number-of-buttons]
   (dotimes [n number-of-buttons]
     (.add panel (create-button (- number-of-buttons n)))))
 
-(def panel (doto (JPanel.)
-                 #(.setLayout % (BoxLayout. % BoxLayout/Y_AXIS))
-                 (add-buttons 5)))
+(defn create-and-set-panel [frame floors]
+  (.setContentPane frame
+    (doto (JPanel.)
+          (.setLayout (GridLayout. 0 1))
+          (add-buttons floors))))
 
-(def frame (doto (JFrame. "Elevator")
+(def frame (doto (JFrame. "Elevator control panel")
                  (.setMinimumSize (Dimension. 100 300))
-                 (.setContentPane panel)
+                 (create-and-set-panel (get-number-of-floors))
                  (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)))
-;(.revalidate button)
 
 (defn format-response [floor-to-go]
   (json/generate-string {:go-to floor-to-go}))
@@ -49,6 +55,18 @@
 (defn open-window []
   (.setVisible frame true))
 
-;TODO update buttons based on floors in state
+(defn update-button-amount [floors]
+  (do
+    (set-number-of-floors floors)
+    (doto
+      frame
+      (create-and-set-panel floors)
+      (.setMinimumSize (Dimension. 100 (* 50 floors)))
+      (.pack))))
+
 (defn decide-floor-to-go [state]
-  (format-response (get-target)))
+  (do
+    (let [floors-in-state (:floors state)]
+      (when-not (= floors-in-state (get-number-of-floors))
+        (update-button-amount floors-in-state)))
+    (format-response (get-target))))
